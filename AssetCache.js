57447097e1156e3e5a1ea2ac7ab8cb4fac14acb6
Ryan Corradini <ryancorradini@yahoo.com>
2012-07-12 08:10:47 -0400

    Finished AssetCache, added tutorial page

--- AssetCache.js
+++ AssetCache.js
@@ -7,59 +7,89 @@
 
 
 define(
-    ['atto/event'],
-    function(Event) {
+    ['atto/core','atto/event'],
+    function(atto,CustomEvent) {
         function constructor(args) {
             var _assets     = {},
                 _loadStatus = {},
                 _events = {
-                    error:  new Event('tiger.assetCache.error'),
-                    ready:  new Event('tiger.assetCache.ready'),
-                    loaded: new Event('tiger.assetCache.loaded'),
+                    error:  new CustomEvent('tangle.assetCache.error'),
+                    ready:  new CustomEvent('tangle.assetCache.ready'),
+                    loaded: new CustomEvent('tangle.assetCache.loaded'),
+                },
+                _types = {
+                    IMAGE: 1,
+                    AUDIO: 2
+                },
+                _extension_to_type = {
+                    png: 1,
+                    bmp: 1,
+                    gif: 1,
+                    jpg: 1,
+                    jpeg: 1,
+                    wav: 2,
+                    webm: 2,
+                    ogg: 2,
+                    mp3: 2,
+                    aac: 2
                 };
 
-            function _addAsset(name, src, type) {
-                var self = this;
+            function _assetTypeFromName(assetName) {
+                var retVal;
+                if (assetName.indexOf('.') > -1) {
+                    // lastIndexOf is okay because all canvas-aware browsers have it
+                    var ext = assetName.substr(assetName.lastIndexOf('.')+1);
+                    retVal = _extension_to_type[ext.toLowerCase()];
+                }
+
+                // default to image if we couldn't figure it out
+                return retVal || _types.IMAGE;
+            }
+
+            function _createLoadCallback(assetName) {
+                return function() {
+                    _loadStatus[assetName] = true;
+                    _events.loaded.dispatch({name:assetName});
+
+                    if (_ready()) {
+                        _events.ready.dispatch({});
+                    }
+                }
+            }
+
+            function _createErrorCallback(assetName) {
+                return function(e) {
+                    _loadStatus[assetName] = false;
+                    _events.error.dispatch({name:assetName, details:"Asset '"+assetName+"' could not be loaded."});
+                }
+            }
+
+            function _addAsset(name, src) {
+                var type = _assetTypeFromName(src);
+
                 if (name in _assets) {
-                    //console.log("Asset '",name,"' is already in the cache");
+                    _events.error.dispatch({name:name, details:"Asset '"+name+"' is already in the cache"});
                     return;
                 } else {
 
-                    if (type === 'audio') {
+                    if (type === _types.IMAGE) {
+                        // image: this is by far the simple case
+                        _assets[name] = new Image();
+                        //_assets[name].onload = _createLoadCallback(name);
+                        atto.addEvent(_assets[name], 'load', _createLoadCallback(name), false);
+                        //_assets[name].onerror = _createErrorCallback(name);
+                        atto.addEvent(_assets[name], 'error', _createErrorCallback(name), false);
+
+                    } else if (type === _types.AUDIO) {
+                        // audio; this will take a little more magic, due to browser codec issues
                         _assets[name] = new Audio();
-
-                        _assets[name].onerror = function(e) {
-                            _loadStatus[name] = false;
-                            var msg = "Asset named '" + name + "' could not be loaded.";
-                            _events.error.dispatch({name:name, details:msg});
-                        }
-
-                        _assets[name].addEventListener('canplaythrough', function() {
-                            _loadStatus[name] = true;
-                            _events.loaded.dispatch({name:name});
-
-                            if (_ready()) {
-                                _events.ready.dispatch({});
-                            }
-                        }, false);
+                        atto.addEvent(_assets[name], 'canplaythrough', _createLoadCallback(name), false);
+                        atto.addEvent(_assets[name], 'error', _createErrorCallback(name), false);
 
                     } else {
-                        _assets[name] = new Image();
-
-                        _assets[name].onerror = function(e) {
-                            _loadStatus[name] = false;
-                            var msg = "Asset named '" + name + "' could not be loaded.";
-                            _events.error.dispatch({name:name, details:msg});
-                        }
-
-                        _assets[name].onload = function() {
-                            _loadStatus[name] = true;
-                            _events.loaded.dispatch({name:name});
-
-                            if (_ready()) {
-                                _events.ready.dispatch({});
-                            }
-                        }
+                        // unsupported asset type
+                        _events.error.dispatch({name:name, details:"Asset type '"+type+"' not supported."});
+                        return false;
                     }
 
                     _loadStatus[name] = false;
@@ -75,10 +105,22 @@ define(
                 if (_assets[name]) {
                     return _assets[name];
                 } else {
-                    var msg = "Asset named '" + name + "' not found in cache.";
-                    _events.error.dispatch({name:name, details:msg});
+                    _events.error.dispatch({name:name, details:"Asset named '"+name+"' not found in cache."});
                     return null;
                 }
+            }
+
+            function _getAssetType(name) {
+                var obj;
+                if (_assets[name]) {
+                    obj = _assets[name];
+                    if (obj) {
+                        return (obj.tagName === "IMG") ? _types.IMAGE : _types.AUDIO;
+                    }
+                } else {
+                    _events.error.dispatch({name:name, details:"Asset named '"+name+"' not found in cache."});
+                }
+                return -1;
             }
 
             function _ready() {
@@ -94,11 +136,13 @@ define(
             }
 
             return {
-                addAsset : _addAsset,
-                hasAsset : _hasAsset,
-                getAsset : _getAsset,
-                ready    : _ready,
-                events   : _events
+                TYPES        : _types,
+                addAsset     : _addAsset,
+                hasAsset     : _hasAsset,
+                getAsset     : _getAsset,
+                getAssetType : _getAssetType,
+                ready        : _ready,
+                events       : _events
             } // end of public interface
         } // end of constructor
 
--- README.md
+++ README.md
@@ -1,31 +1,31 @@
 TangleJS: A Tiny Game Library
 -------------
-Tangle is my contribution to the growing pile of small, lightweight HTML5 game libraries. It's mostly shared here as an educational effort, in conjunction with the [Let's Make a Canvas Library](http://www.buyog.com/code/search.php?tag=lmacl) series on my blog.
+Tangle is my contribution to the growing pile of small, lightweight HTML5 game libraries. It's mostly shared here as an educational effort, in conjunction with the [Let's Make a Canvas Library](http://www.buyog.com/code/search.php?tag=lmacl) series on my blog, and in accordance with the principles outlined in [this presentation](www.buyog.com/talks/utahjs) I gave at UtahJS in June 2012.
 
 
 Modules
 -----
 
-Tangle is built upon the [Asynchronous Module Definition API](https://github.com/amdjs/amdjs-api/wiki/AMD), commonly referred to as AMD. This means that dependencies are taken care of for free, and we don't need a preloader to bootstrap our JavaScript code for us (other than an AMD-compatible loader, of course).
+Tangle is built upon the [Asynchronous Module Definition API](https://github.com/amdjs/amdjs-api/wiki/AMD), commonly referred to as AMD. This means that script dependencies are taken care of more or less for free, and we don't need a separate preloading step to bootstrap our JavaScript code for us (other than an [AMD-compatible](requirejs.org) [loader](https://github.com/cujojs/curl), of course).
 
-Tangle is built upon some basic functionality present in [Atto](https://github.com/buyog/atto), my minimal JavaScript framework, and relies on a few core elements of that project. As some point I may copy them in here to make this library self-contained, but that isn't a priority at this time.
+Tangle is built upon some basic functionality provided by [Atto](https://github.com/buyog/atto), my minimal JavaScript library, and relies on a few core elements of that project. As some point I may copy them in here to make this library self-contained, but that isn't a priority at this time (remember, this library is educational in nature, not production-ready).
 
-Beyond script module loading, Tangle takes care of all other game dependencies: images, sprite sheets, music and sounds, and so on. Each of these object types has (or will eventually have) its own module as part of the library:
+Beyond script module loading, Tangle aims to provide many of the basic building blocks for making a game in HTML5: sprite sheets and animation, stage backgrounds and viewports, music and sounds, and so on. Each of these discrete functional pieces has (or will eventually have) its own module as part of the library:
 
   * AssetCache (image/audio file preloader)
   * Sprite
   * Map
-  * Screen
+  * Screen (viewport-sized subsection of a Map)
   * SoundEffect
   * Music
   * Font
+  * etc...
 
-Additionally, Tangle will provide modules to help structure a game's code:
+Additionally, I intend Tangle to eventually provide modules to help structure a game's logic:
 
-  * Physics engine
-  * Collision detection
   * Game loop
-  * TBD
+  * Collision detection
+  * Physics engine
+  * etc...
 
 It is likely that this module list will evolve over the course of the blog series, so check back often.
-
