606db333be4fc5b33dc7344bf780de294ab8a562
Ryan Corradini <ryan@buyog.com>
2013-05-28 03:06:36 -0400

    merge

--- core.js
+++ core.js
@@ -64,8 +64,8 @@ define([], function() {
     // https://developer.mozilla.org/en-US/docs/Web/Guide/User_experience/Using_the_Page_Visibility_API
     (function(pv){
         // Set the name of the hidden property and the change event for visibility
-        var hidden, visibilityChange; 
-        if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support 
+        var hidden, visibilityChange;
+        if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
             pv.hidden = "hidden";
             pv.visibilityChange = "visibilitychange";
         } else if (typeof document.mozHidden !== "undefined") {
